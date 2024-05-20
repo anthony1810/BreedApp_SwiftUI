31d38365d3e27fa42c989c3e8decf26d89e25e11
Quang Tran <quang.tran@appetiser.com.au>
2024-05-20 11:03:30 +0700

    Initial Commit

---
+++ MVPattern/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVPattern
+//
+//  Created by Anthony Tran on 20/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MVPattern/MVPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVPatternApp.swift
+//  MVPattern
+//
+//  Created by Anthony Tran on 20/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MVPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
